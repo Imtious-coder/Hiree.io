7a7661b4602f081388e47b611145bbd7502a2587
Imtious-Coder <iarafat386@gmail.com>
2023-03-19 10:58:58 +0600

    navbar issue fixed

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Meta from "../../components/Meta/Meta";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <>
-      <Meta title={"Hiree.io"} />
-      <nav class="header navbar navbar-expand-lg bg-dark py-4">
-        <div class="container-fluid">
-          <Link to="/" class="logo">
+      <nav className="header navbar navbar-expand-lg bg-dark py-4">
+        <div className="container-fluid">
+          <Link to="/" className="logo">
             Hiree.io
           </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -21,28 +19,28 @@ const Header = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse px-auto" id="navbarNav">
-            <ul class="navbar-nav ms-auto mt-3 mt-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto mt-3 mt-lg-0">
+              <li className="nav-item">
                 <Link
                   to="/"
-                  class="nav-link active text-center"
+                  className="nav-link active text-center"
                   aria-current="page"
                 >
                   Home
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link to="/" class="nav-link">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
                   Recruiters
                 </Link>
               </li>
-              <li class="nav-item dropdown">
+              <li className="nav-item dropdown">
                 <Link
                   to="/"
-                  class="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle"
                   href="#cs"
                   role="button"
                   data-bs-toggle="dropdown"
@@ -50,30 +48,30 @@ const Header = () => {
                 >
                   Success Stories
                 </Link>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                   <li>
-                    <Link to="/" class="dropdown-item">
+                    <Link to="/" className="dropdown-item">
                       Employers who hired top talents
                     </Link>
                   </li>
                   <li>
-                    <Link to="/" class="dropdown-item">
+                    <Link to="/" className="dropdown-item">
                       recruiters who earned thousands
                     </Link>
                   </li>
                 </ul>
               </li>
-              <li class="nav-item">
-                <Link to="/" class="nav-link">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
                   Career
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link to="/" class="nav-link">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
                   Contact us
                 </Link>
               </li>
-              <li class="nav-item mx-lg-4 mt-3 mt-lg-0">
+              <li className="nav-item mx-lg-4 mt-3 mt-lg-0">
                 <div className="d-flex gap-4 justify-content-center align-items-center">
                   <button>Login</button>
                   <button>Sign up</button>
