75c4f9934fb52186ca2895e28cc3ec013e9877e4
Imtious-Coder <iarafat386@gmail.com>
2023-03-19 12:03:17 +0600

    Hero section complete

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import image1 from "../../Assets/Images/hero1.jpg";
+import image2 from "../../Assets/Images/hero2.webp";
+import image3 from "../../Assets/Images/hero3.webp";
 import Meta from "../../components/Meta/Meta";
+import "./Style.scss";
 
 const index = () => {
   return (
     <>
       <Meta title={"Hiree.io"} />
-      {/* <div
+      {/* HERO SECTION */}
+      <section
         id="carouselExampleDark"
-        class="carousel carousel-dark slide"
+        className="hero carousel slide"
         data-bs-ride="carousel"
       >
-        <div class="carousel-indicators">
+        <div className="carousel-indicators">
           <button
             type="button"
             data-bs-target="#carouselExampleDark"
             data-bs-slide-to="0"
-            class="active"
+            className="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
@@ -32,66 +37,73 @@ const index = () => {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
-            <img
-              src="https://unsplash.com/images/stock/high-resolution"
-              class="d-block w-100"
-              alt="..."
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src={image1} className="" alt="..." />
+            <div className="details carousel-caption">
+              <div>
+                <h4>HARNESS THE POWER OF THE CROWD</h4>
+                <h1>TO FILL JOB OPENINGS</h1>
+                <div className="d-flex gap-5 mt-4 align-items-center justify-content-center">
+                  <button className="button1">Watch a demo</button>
+                  <button className="button2">discover more</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img
-              src="https://unsplash.com/images/stock/high-resolution"
-              class="d-block w-100"
-              alt="..."
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={image2} className="" alt="..." />
+            <div className="details carousel-caption">
+              <div>
+                <h4>TALENT ACQUISITION SOLUTION TO HELP YOU</h4>
+                <h1>HIRE THE MOST QUALIFIED CANDIDATESS</h1>
+                <div className="d-flex gap-5 mt-4 align-items-center justify-content-center">
+                  <button className="button1">Watch a demo</button>
+                  <button className="button2">discover more</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <img
-              src="https://unsplash.com/images/stock/high-resolution"
-              class="d-block w-100"
-              alt="..."
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
+          <div className="carousel-item">
+            <img src={image3} className="" alt="..." />
+            <div className="details carousel-caption">
+              <div>
+                <h4>MEET THE BEST TALENT</h4>
+                <h1>WORK WITH THEM EASILY</h1>
+                <div className="d-flex gap-5 mt-4 align-items-center justify-content-center">
+                  <button className="button1">Watch a demo</button>
+                  <button className="button2">discover more</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExampleDark"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExampleDark"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
-      </div> */}
+      </section>
+      {/* TRUSTED SECTION */}
     </>
   );
 };
